ceb15dd69793831a87b0b81fee2d59c51ab20122
Anne-Sophie Cusson <64850625+4nn3-50ph13@users.noreply.github.com>
2022-03-08 18:42:47 -0500

    Starting Patient ui

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Patient from './PatientComponents';
+import Patient from './Patient';
 import '@fontsource/roboto/300.css';
 
 ReactDOM.render(
